3d8aa38dc918016b4353796171ac9de9f915cc88
blawidris <blawidris@gmail.com>
2023-02-06 05:25:32 +0100

    operators'

--- chapter/02_operators/index.php
+++ chapter/02_operators/index.php
@@ -51,21 +51,36 @@ OPERATORS
 
 // Comparison
 
-$x = 10;
-$y = 2;
+// $x = 10;
+// $y = 2;
 
 // equal
-var_dump($x == 10); # not strict
-var_dump($x === '10'); # strict
+// var_dump($x == 10); # not strict
+// var_dump($x === '10'); # strict
 
 // not equal
-var_dump($y != '2'); # not strict
-var_dump($y !== '2'); # strict
+// var_dump($y != '2'); # not strict
+// var_dump($y !== '2'); # strict
 
-var_dump($x > $y); # greater than
-var_dump($x < $y); # less than
-var_dump($x >= $y); # greater than or equal to
-var_dump($x <= $y); # less than or equal to
-var_dump($x <=> 10);  # spaceship 0
-var_dump($x <=> 20);  # spaceship -1
-var_dump($x <=> $y);  # spaceship 1
+// var_dump($x > $y); # greater than
+// var_dump($x < $y); # less than
+// var_dump($x >= $y); # greater than or equal to
+// var_dump($x <= $y); # less than or equal to
+// var_dump($x <=> 10);  # spaceship 0
+// var_dump($x <=> 20);  # spaceship -1
+// var_dump($x <=> $y);  # spaceship 1
+
+// Ternary Operators
+// $x = "Hello World";
+// $y = strpos($x, 'H');
+
+// $result = $y === false ? 'H not found' : "H found at index {$y}";
+// echo $result . PHP_EOL;
+
+ 
+// Coalescing Operators
+$x = null;
+
+$y = $x ?? "Hello";
+
+print_r($y . PHP_EOL);
