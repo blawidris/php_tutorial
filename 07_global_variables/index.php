ac2d6051b2bf80de1cc5245b815631fc11b1b0e7
blawidris <blawidris@gmail.com>
2023-03-08 21:35:28 +0100

    global variable

--- 07_global_variables/index.php
+++ 07_global_variables/index.php
@@ -4,12 +4,49 @@
 // var_dump($_ENV);
 // echo "</pre>";
 
-echo "<pre>";
-var_dump($_GET);
-echo "</pre>";
+// echo "<pre>";
+// var_dump($_GET);
+// echo "</pre>";
 
-// $_POST
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_SERVER);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_REQUEST);
+// echo "</pre>";
+
+
+
+// setcookie('myName', 'Idris', time() + 3600 +3600);
+
+// echo "<pre>";
+// var_dump($_COOKIE['myName']);
+// echo "</pre>";\
+
+// session_start();
+
+// $_SESSION['username'] = 'blawidris';
+
+// echo "<pre>";
+// var_dump($_SESSION);
+// echo "</pre>";
+
+// session_unset();
+
 // $_SERVER
 // $_REQUEST
 // $_COOKIE
 // $_SESSION
+
+?>
+
+<form action="" method="post">
+
+<input type="text" name="username">
+<button>Submit</button>
+</form>
